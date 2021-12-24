a240815da144f491d6a4e718ea9356fc66328eb4
peakpunk <alk357951@gmail.com>
2021-12-24 17:36:55 +0700

    complete lab 13 edit 13_1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,14 +1,17 @@
 
 #include<iostream>
+#include <ctime>
 using namespace std;
 
 long long int fibonacci(int);
 
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
 }
 
 long long int fibonacci(int x){
@@ -16,3 +19,4 @@ long long int fibonacci(int x){
     else if (x == 0) return 0;
     else return fibonacci(x-1) + fibonacci(x-2);
 }
+

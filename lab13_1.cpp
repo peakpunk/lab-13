ea0c9174d5187501b91099a1834dbe8a72bc4822
peakpunk <alk357951@gmail.com>
2021-12-24 17:30:27 +0700

    complete lab 13

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,18 @@
+
 #include<iostream>
 using namespace std;
+
+long long int fibonacci(int);
 
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+long long int fibonacci(int x){
+    if (x == 1) return 1;
+    else if (x == 0) return 0;
+    else return fibonacci(x-1) + fibonacci(x-2);
 }
